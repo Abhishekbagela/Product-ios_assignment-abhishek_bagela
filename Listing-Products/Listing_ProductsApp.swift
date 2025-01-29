5ac8d108c6deafc9f0a4a31c3ad713f4d5b83128
Abhishek-ab@ios-dev <abhibagela2015@gmail.com>
2025-01-29 21:32:20 +0530

    Initial Commit

---
+++ Listing-Products/Listing_ProductsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Listing_ProductsApp.swift
+//  Listing-Products
+//
+//  Created by Abhishek Bagela on 29/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Listing_ProductsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
